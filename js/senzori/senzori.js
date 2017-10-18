8cf000d455057fff3eb7c2e849ec3dbb727f557a
oanceamaria <oanceamarianicoleta@gmail.com>
2017-10-18 19:35:04 +0300

    rotunjire la doua zecimale

--- js/senzori/senzori.js
+++ js/senzori/senzori.js
@@ -1,8 +1,8 @@
 window.addEventListener('deviceorientation',ondeviceorientation );
 
 function ondeviceorientation(event) { 
-	document.getElementById("id_alpha").innerHTML = "Alpha: " + event.alpha;
-	document.getElementById("id_beta").innerHTML = "Beta: " + event.beta;
-	document.getElementById("id_gama").innerHTML = "Gama: " + event.gamma;
+	document.getElementById("id_alpha").innerHTML = "Alpha: " + Math.round(event.alpha * 100) / 100;
+	document.getElementById("id_beta").innerHTML = "Beta: " + Math.round(event.beta * 100) / 100;
+	document.getElementById("id_gama").innerHTML = "Gama: " + Math.round(event.gamma * 100) / 100;
 	
 }
