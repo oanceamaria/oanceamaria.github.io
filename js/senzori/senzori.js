a142c9feab7518987001f10a6367332170cd9b77
oanceamaria <oanceamarianicoleta@gmail.com>
2017-10-18 19:24:24 +0300

    modificari

--- js/senzori/senzori.js
+++ js/senzori/senzori.js
@@ -3,6 +3,6 @@ window.addEventListener('deviceorientation',ondeviceorientation );
 function ondeviceorientation(event) { 
 	document.getElementById("id_alpha").innerHTML = "Alpha: " + event.alpha;
 	document.getElementById("id_beta").innerHTML = "Beta: " + event.beta;
-	document.getElementById("id_gama").innerHTML = "Gama: " + event.gama;
+	document.getElementById("id_gama").innerHTML = "Gama: " + event.gamma;
 	
 }
