ecd20cac5731eeae6bb9d6f98d7811bbd2b9d85b
oanceamaria <oanceamarianicoleta@gmail.com>
2017-10-25 19:24:29 +0300

    redenumire variabile

--- js/senzori/senzori.js
+++ js/senzori/senzori.js
@@ -1,4 +1,4 @@
-document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.10.25.1"
+document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.10.25.3"
 
 window.addEventListener('deviceorientation',ondeviceorientation );
 window.addEventListener('devicemotion',ondevicemotion );
@@ -12,9 +12,9 @@ function ondeviceorientation(event) {
 function ondevicemotion(event){
 	document.getElementById("acc").innerHTML = "Aceleration >>>  X: " + (Math.round(event.acceleration.x * 10) / 10) + " Y: " + (Math.round(event.acceleration.y * 10) / 10) + " Z: " + (Math.round(event.acceleration.z * 10) / 10);
 	var ag = event.accelerationIncludingGravity;
-	var xInclin = Math.atan(ag.x / ag.z) * 180 / Math.PI;
-	var yInclin = Math.atan(ag.y / ag.z) * 180 / Math.PI;
+	var gamma = - Math.atan(ag.x / ag.z) * 180 / Math.PI;
+	var beta = Math.atan(ag.y / ag.z) * 180 / Math.PI;
 	
 	document.getElementById("accG").innerHTML = "AcelerationG >>>  X: " + (Math.round(ag.x * 10) / 10) + " Y: " + (Math.round(ag.y * 10) / 10) + " Z: " + (Math.round(ag.z * 10) / 10) + 
-												"<br> Inaclinare >>> X: " + (Math.round(xInclin * 10) / 10) + " Y: " + (Math.round(yInclin * 10) / 10);
+												"<br> Inaclinare >>> Gamma: " + (Math.round(gamma * 10) / 10) + " Beta: " + (Math.round(beta * 10) / 10);
 }
