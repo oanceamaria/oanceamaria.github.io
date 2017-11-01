722c071731ff77f0b24f3f06eb9da15bf6024954
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-01 17:55:06 +0200

    cerc

--- js/desen/canvas.js
+++ js/desen/canvas.js
@@ -4,8 +4,7 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
 context.beginPath();
-context.moveTo(10, 10);
-context.lineTo(100, 200);
-context.lineTo(200, 100);
-context.closePath();
+var w = canvas.getAttribute("width");
+var h = canvas.getAttribute("height");
+context.arc(w / 2, h / 2, 10, 0, 2 * Math.PI);
 context.stroke();
