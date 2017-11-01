325515c599a048aa0b2224949bbcdb5f9cbaacb4
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-01 18:55:00 +0200

    exemplu svg

---
+++ js/svg/svg.js
@@ -0,0 +1,8 @@
+document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.01.0"
+
+var cerc = document.getElementById("cerc");
+cerc.setAttribute("cx", 100);
+cerc.setAttribute("cy", 200);
+cerc.setAttribute("r", 20);
+
+
