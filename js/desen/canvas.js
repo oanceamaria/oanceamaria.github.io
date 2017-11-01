8a48ea29a89f17bff21f01880b4734b31cf6e374
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-01 17:45:46 +0200

    close path

--- js/desen/canvas.js
+++ js/desen/canvas.js
@@ -1,4 +1,4 @@
-document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.01.0"
+document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.01.1"
 
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
@@ -7,4 +7,5 @@ context.beginPath();
 context.moveTo(10, 10);
 context.lineTo(100, 200);
 context.lineTo(200, 100);
+context.closePath();
 context.stroke();
