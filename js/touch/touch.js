47beb41f87c153048bfb40b1d24bc1d620c83e3f
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-08 18:16:10 +0200

    culoare aleatoare

--- js/touch/touch.js
+++ js/touch/touch.js
@@ -5,6 +5,15 @@ var context = canvas.getContext('2d');
 
 canvas.addEventListener("touchstart", onTouchStart);
 
+function genRendColor(){
+	var litere = "0123456789ABCDEF"
+	var color = "#";
+	for (i = 0; i < 6; i++){
+		color += litere[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
 function onTouchStart(event){
 	var touches = event.changedTouches;
 	var rect = canvas.getBoundingClientRect();
@@ -12,7 +21,7 @@ function onTouchStart(event){
 	for (i = 0; i < touches.length; i++)
 		context.beginPath();
 		context.arc(touches[i].pageX - rect.left, touches[i].pageY - rect.top, 10, 0, 2 * Math.PI);
+		context.strokeStyle = genRendColor();
 		context.stroke();
 	}
 	
-}
