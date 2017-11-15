cd0d8c3ee72e19f53e0244ef469cc2e67893b031
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-15 19:12:49 +0200

    circle inside box

--- js/svgTouch/svgMove.js
+++ js/svgTouch/svgMove.js
@@ -11,13 +11,18 @@ var svg = document.getElementById("svg");
 var rect = svg.getBoundingClientRect();
 
 function onTouchMove(e){
+	e.preventDefault();
 	var touches = e.changedTouches;
 	
 	for (var i = 0; i < touches.length; i++){
 		var circle = touches[i].target;
-		
-		circle.setAttribute("cx", touches[i].pageX - rect.left);
-		circle.setAttribute("cy", touches[i].pageY - rect.top);	
+		if( touches[i].pageX - rect.left <= rect.width  &&
+			touches[i].pageY - rect.top <= rect.height  &&
+			touches[i].pageX - rect.left >= 0  &&
+			touches[i].pageY - rect.top >= 0 ){
+			circle.setAttribute("cx", touches[i].pageX - rect.left);
+			circle.setAttribute("cy", touches[i].pageY - rect.top);	
+		}
 	}
 		
 }
