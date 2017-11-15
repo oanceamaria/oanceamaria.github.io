fb3836e748896b3eca2d7f229cc5d24557f78213
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-15 18:17:45 +0200

    coordonate

--- js/svgTouch/svgTouch.js
+++ js/svgTouch/svgTouch.js
@@ -1,7 +1,9 @@
-document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.15.1"
-
+document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.15.2"
 
 addEventListener("touchstart", onTouchStart);
+
+var svg = document.getElementById("svg");
+var rect = svg.getBoundingClientRect();
 
 function onTouchStart(e){
 	var touches = e.changedTouches;
@@ -9,10 +11,9 @@ function onTouchStart(e){
 	for (var i = 0; i < touches.length; i++){
 		var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 		
-		circle.setAttribute("cx", touches[i].pageX);
-		circle.setAttribute("cy", touches[i].pageY);
-		circle.setAttribute("r", 10);
-		var svg = document.getElementById("svg");
+		circle.setAttribute("cx", touches[i].pageX - rect.left);
+		circle.setAttribute("cy", touches[i].pageY - rect.top);
+		circle.setAttribute("r", 10);	
 		svg.appendChild(circle);
 	}
 		
