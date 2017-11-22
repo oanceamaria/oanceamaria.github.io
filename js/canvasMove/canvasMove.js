634a8a3b7d523dfd54aaab8824234d3164efdf2e
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-22 17:46:30 +0200

    img

--- js/canvasMove/canvasMove.js
+++ js/canvasMove/canvasMove.js
@@ -1,12 +1,18 @@
-document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.15.0";
+document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.22.0";
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+var rectCanvas = canvas.getBoundingClientRect();
 var img = document.getElementById("img");
+var topX = 100;
+var topY = 100;
+var imgWidth = 100;
+var imgHeight = 100;
+
 
 img.onload = function (){
 	var rect = img.getBoundingClientRect();
-	context.drawImage(img, 100, 100, 100, 100);
+	context.drawImage(img, topX, topY, imgWidth, imgHeight);
 }
 canvas.addEventListener("touchmove", onTouchMove);
 
@@ -15,14 +21,12 @@ function onTouchMove(e){
 	var touches = e.changedTouches;
 	
 	for (var i = 0; i < touches.length; i++){
-		var circle = touches[i].target;
-		if( touches[i].pageX - rect.left <= rect.width  &&
-			touches[i].pageY - rect.top <= rect.height  &&
-			touches[i].pageX - rect.left >= 0  &&
-			touches[i].pageY - rect.top >= 0 ){
-				
-			circle.setAttribute("cx", touches[i].pageX - rect.left);
-			circle.setAttribute("cy", touches[i].pageY - rect.top);	
+		if( touches[i].pageX < topX + imgWidth  && touches[i].pageX >= topX &&
+			touches[i].pageY < topY + imgHeight  && touches[i].pageY >= topY ) {
+				context.clearRect();
+				topX = touches[i].pageX - rectCanvas.left;
+				topY = touches[i].pageY - rectCanvas.top;
+				context.drawImage(img, topX, topY, imgWidth, imgHeight);
 		}
 	}
 		
