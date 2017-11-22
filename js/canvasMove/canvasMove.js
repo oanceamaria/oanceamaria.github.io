ca14cf9fa24352067c5ec0e0353c6e0bac0278fa
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-22 18:10:24 +0200

    better position

--- js/canvasMove/canvasMove.js
+++ js/canvasMove/canvasMove.js
@@ -1,4 +1,4 @@
-document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.22.3";
+document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.22.4";
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
@@ -23,8 +23,8 @@ function onTouchMove(e){
 	for (var i = 0; i < touches.length; i++){
 		if( touches[i].pageX - rectCanvas.left < topX + imgWidth  && touches[i].pageX - rectCanvas.left >= topX &&
 			touches[i].pageY - rectCanvas.top < topY + imgHeight  && touches[i].pageY - rectCanvas.top >= topY ) {
-				var offsetInsideImgX = topX - ( touches[i].pageX - rectCanvas.left );
-				var offsetInsideImgY = topY - ( touches[i].pageY - rectCanvas.top );
+				var offsetInsideImgX = ( touches[i].pageX - rectCanvas.left ) - topX ;
+				var offsetInsideImgY = ( touches[i].pageY - rectCanvas.top ) - topY ;
 				context.clearRect(0, 0, 800, 600 );
 				topX = touches[i].pageX - rectCanvas.left - offsetInsideImgX;
 				topY = touches[i].pageY - rectCanvas.top - offsetInsideImgY;
