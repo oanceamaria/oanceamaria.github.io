9d8d6235f0266121bdbfb6a6e93f88605eac374c
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-22 18:54:00 +0200

    m

--- js/synt/synt.js
+++ js/synt/synt.js
@@ -1,10 +1,10 @@
-document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.22.0";
+document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.22.1";
 
 var synt = window.speechSynthesis;
 
 function getVoices(){
-	var voices = synt.getVoices;
-	for (int i = 0; i < voices.length; i++){
+	var voices = synt.getVoices();
+	for (var i = 0; i < voices.length; i++){
 		document.getElementById("idVoices").innerHTML += voices[i].name + ": " + voices[i].lang + "<br>";
 	}
 }
