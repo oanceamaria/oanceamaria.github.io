3dc4dcb4537111f9fab591a684e7a928478b6bd0
oanceamaria <oanceamarianicoleta@gmail.com>
2017-11-29 18:57:29 +0200

    speech

---
+++ js/speech/speech.js
@@ -0,0 +1,21 @@
+document.getElementById("idLogicV").innerHTML = "Logic level version: 2017.11.29.0";
+
+document.addEventListener("touchstart", onTouchStart);
+
+var recognition = new webkitSpeechRecognition();
+
+recognition.lang = "en-US";
+recognition.onresult = onSpeechResult;
+recognition.onsoundend = onSoundEnd;
+
+function onTouchStart(e){
+	recognition.start();
+}
+
+function onSpeechResult(e){
+	document.getElementById("id_p").innerHTML = e.results[0][0].transcript;
+}
+
+function onSoundEnd(e){
+	recognition.stop();
+}
