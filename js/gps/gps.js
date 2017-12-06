c138fc797cf3782cbe99bd8082563e8fabcbd10c
oanceamaria <oanceamarianicoleta@gmail.com>
2017-12-06 18:13:53 +0200

    harta

--- js/gps/gps.js
+++ js/gps/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_level_version").innerHTML = "Business level version: 2017.12.06.1"; 
+document.getElementById("idLogicV").innerHTML = "Business level version: 2017.12.06.2"; 
 
 
 
@@ -8,7 +8,13 @@ navigator.geolocation.getCurrentPosition(on_gps_success, on_gps_error);
 //--------------------------------------
 function on_gps_success(p)
 {
-	document.getElementById("id_p").innerHTML = "lat = " + p.coords.latitude + " long = " + p.coords.longitude + " accuracy = " + p.coords.accuracy + "m";
+	document.getElementById("id_p").innerHTML = "lat = " + p.coords.latitude + "m" +
+	" <br> long = " + p.coords.longitude + "m" +
+	" <br> accuracy = " + p.coords.accuracy + "m" +
+	" <br> altitude = " + p.coords.altitude + "m";
+	
+	var mapStr = "https://maps.googleapis.com/maps/api/staticmap?center=" + p.coords.latitude + "," + p.coords.longitude + "&zoom=10&size=320x240&key=AIzaSyCG5pbBXwZYliEtdrfQWsLMCa_6EYmqCIk";
+	document.getElementById(idGpsImg).setAttribute("src", mapStr);
 } 
 //--------------------------------------
 function on_gps_error(e)
