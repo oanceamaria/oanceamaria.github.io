4835e03ad70ecbb524ca020db0c645b42c8bc5a6
oanceamaria <oanceamarianicoleta@gmail.com>
2017-12-06 18:21:09 +0200

    marker2

--- js/gps/gps.js
+++ js/gps/gps.js
@@ -1,4 +1,4 @@
-document.getElementById("idLogicV").innerHTML = "Business level version: 2017.12.06.2"; 
+document.getElementById("idLogicV").innerHTML = "Business level version: 2017.12.06.3"; 
 
 
 
@@ -13,7 +13,7 @@ function on_gps_success(p)
 	" <br> accuracy = " + p.coords.accuracy + "m" +
 	" <br> altitude = " + p.coords.altitude + "m";
 	
-	var mapStr = "https://maps.googleapis.com/maps/api/staticmap?markers=color:blue|" + p.coords.latitude + "," + p.coords.longitude + "&zoom=10&size=320x240&key=AIzaSyCG5pbBXwZYliEtdrfQWsLMCa_6EYmqCIk";
+	var mapStr = "https://maps.googleapis.com/maps/api/staticmap?markers=color:blue|" + p.coords.latitude + "," + p.coords.longitude + "|45,23&zoom=7&size=320x240&key=AIzaSyCG5pbBXwZYliEtdrfQWsLMCa_6EYmqCIk";
 	document.getElementById("idGpsImg").setAttribute("src", mapStr);
 } 
 //--------------------------------------
