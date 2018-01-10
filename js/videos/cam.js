9b70e493c3af47454854a556c4095cae2ff03a26
oanceamaria <oanceamarianicoleta@gmail.com>
2018-01-10 18:00:39 +0200

    camera

---
+++ js/videos/cam.js
@@ -0,0 +1,19 @@
+var d = new Date();
+document.getElementById("idLogicV").innerHTML = "Business level version: " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
+
+var contraints = {audio: true, video:true};
+navigator.mediaDevices.getUserMedia(contraints).then(on_success).catch(on_error);
+
+var video = document.getElementById("videoId");
+
+function on_success(stream){
+	video.srcObject = stream;
+}
+
+function on_error(error){
+	alert("Error");
+}
+
+
+
+
