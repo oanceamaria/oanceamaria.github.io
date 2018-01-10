d777a5013656fe9f9480c16646a6497cab1e1054
oanceamaria <oanceamarianicoleta@gmail.com>
2018-01-10 18:10:36 +0200

    cam spate

--- js/videos/cam.js
+++ js/videos/cam.js
@@ -1,7 +1,7 @@
 var d = new Date();
 document.getElementById("idLogicV").innerHTML = "Business level version: " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".0"; 
 
-var contraints = {audio: true, video:true};
+var contraints = {audio: true, video:{facingMode:"environment"}};
 navigator.mediaDevices.getUserMedia(contraints).then(on_success).catch(on_error);
 
 var video = document.getElementById("videoId");
